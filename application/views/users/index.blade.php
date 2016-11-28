a7aaed7a640b605d2c653279955ad09a72c89d83
Tim Joosten <Tjoosten3@gmail.com>
2016-11-28 14:25:48 +0100

    Set user overview table

--- application/views/users/index.blade.php
+++ application/views/users/index.blade.php
@@ -22,10 +22,54 @@
                 <div class="box-body">
                     @if (count($users) == 0) {{-- Show info alert --}}
                         <div class="alert alert-info">
-                            <strong><span class="fa fa-info-circle"></span> Info:</strong> 
+                            <strong><span class="fa fa-info-circle"></span> Info:</strong>
                             Er zijn geen gebruikers gevonden in het systeem gevonden.
                         </div>
                     @else {{-- Show users overview --}}
+                        <table class="table table-condensed table-hover">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Status:</th>
+                                    <th>Username:</th>
+                                    <th>Naam:</th>
+                                    <th>Email:</th>
+                                    <th></th> {{-- Reserved for functions --}}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($users as $user)
+                                    <tr>
+                                        <td><code>#U{{ $user->id }}</code></td>
+                                        <td>
+                                            @if ($user->status == 0)
+                                                <span class="label label-success">Actief</span>
+                                            @elseif($user->status == 1)
+                                                <span class="label label-danger">Geblokkeerd</span>
+                                            @else
+                                                <span class="label label-info">Onbekend</span>
+                                            @endif
+                                        </td>
+                                        <td>{{ $user->username }}</td>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->email }}</td>
+
+                                        {{-- Functions --}}
+                                            <td>
+                                                <a href="" class="label label-default">Reset wachtwoord</a>
+                                                <a href="" class="label label-danger">Verwijder</a>
+
+                                                @if ($user->blocked == 0)
+                                                    <a href="" class="label label-warning">Blokkeer</a>
+                                                @elseif ($user->blocked == 1)
+                                                    <aé href="" class="label label-warning">Activeren</a>
+                                                @endif
+                                            </td>
+                                        {{-- /Functions --}}
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     @endif
                 </div>{{-- /.box-body --}}
             </div>{{-- /.box --}}
