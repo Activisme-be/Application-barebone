7fb304920a27eba44b283652a98fefdbb06ae0f4
Tim Joosten <Tjoosten3@gmail.com>
2016-12-01 16:24:39 +0100

    Add profile update method

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -23,7 +23,10 @@
      public function __construct()
      {
          parent::__construct();
+         $this->load->library(['blade', 'session', 'form_validation']);
+         $this->load->helper(['url']);
 
+         $his->User = $this->session->userdata('logged_in');
      }
 
      /**
@@ -52,7 +55,10 @@
       */
      public function index()
      {
+         $data['title'] = 'Profiel';
+         $data['user']  = Login::find($this->User['id']);
 
+         $this->blade->render('account/index', $data);
      }
 
      /**
@@ -63,6 +69,40 @@
       */
      public function insert()
      {
+        $this->form_validation->set_rules('name', 'Name', 'trim|required');
+        $this->form_validation->set_rules('username', 'Username', 'trim|required');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
 
+        if ($this->form_validation->run() === false) { // Form validation fails.
+            // printf(validation_errors());  // For debugging propose.
+            // die();                        // For debugging propose.
+
+            $class   = 'alert alert-alert';
+            $message = 'Wij konden de invoer niet goed verwerken.';
+        } else { // Form validation success
+            // Begin user insert.
+            $user            = Login::find($this->User['id']);
+            $user->name     = $this->input->post('name');
+            $user->username = $this->input->post('username');
+            $user->email    = $this->input->post('email');
+
+            if (! empty($this->input->post('password'))) { // Check if the POST attribute for password is empty.
+                $user->password = $this->input->post('password');
+            }
+
+            if ($user->save()) { // can update the user.
+                $class   = 'alert alert-success';
+                $message = 'Je profiel is successvol aangepast.';
+            } else { // Could not update the user.
+                $class   = 'alert alert-danger';
+                $message = 'Wij konden de gebruiker niet aanpassen.';
+            }
+        }
+
+        // Set flash session data.
+        $this->session->set_flashdata('class', $class);
+        $this->session->set_flashdata('message', $message);
+
+        redirect($_SERVER['HTTP_REFERER']);
      }
  }
