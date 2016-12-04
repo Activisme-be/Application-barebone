b2f2ba53e4cda510499f2972cb62122a476937e4
Tim Joosten <Tjoosten3@gmail.com>
2016-12-04 22:34:44 +0100

    Start embed translations on tickets controllers

---
+++ application/language/english/tickets_lang.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * -----------------------------------------------------------------------------------
+ * TITLE LANGUAGE LINES.
+ * -----------------------------------------------------------------------------------
+ */
+$lang['title_tickets'] = 'Tickets';
+
+/**
+ * -----------------------------------------------------------------------------------
+ * PAGE TITLE LANGUAGE LINES.
+ * -----------------------------------------------------------------------------------
+ */
+$title['page_title_tickets'] = 'Ticket Management';
+
+/**
+ * -----------------------------------------------------------------------------------
+ * PAGE DESCRIPTION LANGUAGE LINES.
+ * -----------------------------------------------------------------------------------
+ */
+$lang['page_description_index'] = 'Ticket module voor activisme BE.';
+$lang['page_description_show']  = 'Ticket informatie';
+
+/**
+ * -----------------------------------------------------------------------------------
+ * FLASH SESSION LANGUAGE LINES.
+ * -----------------------------------------------------------------------------------
+ */
+$lang['flash_insert_success'] = 'Het ticket is aangemaakt.';
+$lang['flash_insert_failed']  = 'Wij konden de creatie van het ticket verwerken.';
+$lang['flash_close_success']  = 'Het ticket is verwijderd.';
+$lang['flash_github_success'] = 'Het ticket is naar github verplaatst.';
+
+/**
+ * -----------------------------------------------------------------------------------
+ * SLACK INTEGRATION LANGUAGE LINES.
+ * -----------------------------------------------------------------------------------
+ */
+$lang['slack_close']  = 'heeft een ticket gesloten.';
+$lang['slack_github'] = 'heeft een ticket doorgeduwd naar github.';
+$lang['slack_insert'] = 'heeft een ticket aangemaakt.';
