6d97095e1f8fbcdce34829b0abc003dca04c4b47
Tjoosten <Tjoosten3@gmail.com>
2016-12-05 02:44:52 +0100

    Update gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,33 +1,45 @@
-var gulp = require('gulp');
+// USED LIBRARIES.
+// ----------------------------------------------
+var gulp       = require('gulp-help')(require('gulp'));
+var livereload = require('gulp-livereload');
 
+// CSS File array
 var CssFiles = [
     './node_modules/admin-lte/dist/css/AdminLTE.css',
     './node_modules/admin-lte/dist/css/skins/skin-blue.css'
 ];
 
-var JsFiles = [
-    './node_modules/admin-lte/dist/js/app.js'
-];
+// Javascript file array.
+var JsFiles = ['./node_modules/admin-lte/dist/js/app.js'];
 
-var ImgFiles = [
-    './node_modules/admin-lte/dist/img/user2-160x160.jpg'
-];
+// Img file array.
+var ImgFiles = ['./node_modules/admin-lte/dist/img/user2-160x160.jpg'];
 
-gulp.task('default', function() {
-  gulp.start('copy-css', 'copy-js', 'copy-avatar');
+// COMMAND: gulp
+gulp.task('default', 'The default task for gulp.', function() {
+    gulp.start('copy-js', 'copy-css', 'copy-avatar');
 });
 
-gulp.task('copy-css', function () {
+// COMMAND: gulp copy-css
+gulp.task('copy-css', 'Copy the css file to the asset folder.', function () {
     gulp.src(CssFiles)
         .pipe(gulp.dest('./assets/css'));
 });
 
-gulp.task('copy-js', function () {
+// COMMAND: gulp copy-js
+gulp.task('copy-js', 'Copy the js files to the asset folder.', function () {
     gulp.src(JsFiles)
-        .pipe(gulp.dest('./assets/js'));
+        .pipe(gulp.dest('./assets/js'))
 });
 
-gulp.task('copy-avatar', function () {
+// COMMAND: gulp copy-avatar
+gulp.task('copy-avatar', 'Copy the default avatar to the asset folder.', function () {
     gulp.src(ImgFiles)
         .pipe(gulp.dest('./assets/img'));
 });
+
+// COMMAND: gulp watch
+// gulp.task('watch', function () {
+//     livereload.listen();
+//     gulp.watch(['dist/**']).on('change', livereload.changed);
+// });
